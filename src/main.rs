42ae998c089a39ed9063966e87d0fb66aa83931f
skyeterran <skyeterran@gmail.com>
2022-11-25 14:57:23 -0800

    Got conditional blocks working via named keys

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,32 @@ use std::io::{self, Write};
 use std::collections::HashMap;
 
 #[derive(Debug)]
-struct Convo {
+struct Convo<'a> {
+    keystore: &'a KeyStore,
     blocks: Vec<Block>,
 }
 
-impl Convo {
+impl<'a> Convo<'a> {
     fn get_line(&self, index: &mut (usize, usize)) -> Option<&String> {
         // Try to get a block and return early if impossible
         let Some(block) = self.blocks.get(index.0) else { return None };
+
+        // Check if the block needs a key
+        if let Some(key) = block.key {
+            // Check the value of this key in the keystore
+            let key_valid = match self.keystore.map.get(key) {
+                Some(v) => *v,
+                None => false
+            };
+
+            // If the key doesn't exist or it's false, move to the next block
+            if !key_valid {
+                index.0 += 1;
+                index.1 = 0;
+                return self.get_line(index);
+            }
+        }
+
         let Some(line) = block.lines.get(index.1) else {
             // We've run out of lines in this block, go to the next one
             index.0 += 1;
@@ -29,6 +47,7 @@ impl Convo {
 
 #[derive(Debug)]
 struct Block {
+    key: Option<&'static str>,
     lines: Vec<String>,
 }
 
@@ -47,22 +66,35 @@ fn main() -> io::Result<()> {
     let mut keys = KeyStore {
         map: HashMap::new(),
     };
+
+    // Add test keys
+    keys.map.insert("ShadowRealm", true);
     
     let mut convo = Convo {
+        keystore: &keys,
         blocks: vec![
             Block {
+                key: None,
                 lines: vec![
                     format!("Oh, hi there!"),
                     format!("What's your name?"),
                 ],
             },
             Block {
+                key: None,
                 lines: vec![
                     format!("This is the second block!"),
                     format!("I'm glad you reached this place.")
                 ],
             },
             Block {
+                key: Some("ShadowRealm"),
+                lines: vec![
+                    format!("Welcome to the shadow realm."),
+                ],
+            },
+            Block {
+                key: None,
                 lines: vec![
                     format!("Now you're in the third block."),
                     format!("Hell yeah, sister.")
