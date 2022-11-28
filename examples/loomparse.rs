2a5167db065907f8450ddd634adec59e177d2819
skyeterran <skyeterran@gmail.com>
2022-11-28 13:42:27 -0800

    Got basic lisp object parsing working

--- examples/loomparse.rs
+++ examples/loomparse.rs
@@ -2,9 +2,16 @@
 
 use std::env;
 use std::fs;
-use loom::parser::tokenize;
+use loom::parser::{tokenize, Object, ParseError};
 
-fn main() {
+fn main() -> Result<(), ParseError> {
     let source = fs::read_to_string("script.loom").expect("Couldn't load file!");
-    println!("{:#?}", tokenize(source));
+    
+    let tokens = tokenize(source)?;
+    //println!("{:#?}", tokens);
+
+    let object = Object::from_tokens(tokens)?;
+    println!("{:#?}", object);
+
+    Ok(())
 }
--- src/parser.rs
+++ src/parser.rs
@@ -20,14 +20,15 @@ fn lispify(source: String) -> String {
 #[derive(Debug)]
 pub enum ParseError {
     UnfinishedString,
+    UnexpectedRParen,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Token {
     LParen,
     RParen,
     Symbol(String),
-    LitString(String),
+    StringToken(String),
 }
 
 /// Takes a Lispy string and turns it into a stream of tokens
@@ -62,7 +63,7 @@ pub fn tokenize(source: String) -> Result<Vec<Token>, ParseError> {
                 if word == "\"" {
                     // Stop consuming the string and move on, clearing the string buffer
                     consume_string = false;
-                    tokens.push(LitString(literal_string.clone()));
+                    tokens.push(StringToken(literal_string.clone()));
                     literal_string = String::new();
                     continue;
                 } else {
@@ -81,4 +82,67 @@ pub fn tokenize(source: String) -> Result<Vec<Token>, ParseError> {
     }
 
     Ok(tokens)
+}
+
+#[derive(Debug)]
+pub enum Object {
+    Symbol(String),
+    LitString(String),
+    List(Vec<Object>),
+}
+
+impl Object {
+    // Creates an object from a stream of tokens
+    pub fn from_tokens(tokens: Vec<Token>) -> Result<Self, ParseError> {
+        use ParseError::*;
+        use Token::*;
+
+        // The list we're building up
+        let mut list: Vec<Object> = Vec::new();
+
+        let mut consume_substream = false;
+        let mut nesting: usize = 0;
+        let mut substream: Vec<Token> = Vec::new();
+        for token in tokens {
+            if !consume_substream {
+                // Token parsing
+                match token {
+                    LParen => {
+                        // Create an object from this sub-expression
+                        consume_substream = true;
+                        nesting += 1;
+                    },
+                    RParen => {
+                        return Err(UnexpectedRParen);
+                    },
+                    Symbol(s) => {
+                        list.push(Object::Symbol(s));
+                    },
+                    StringToken(s) => {
+                        list.push(Object::LitString(s));
+                    },
+                    _ => {},
+                }
+            } else {
+                // Substream parsing
+                match token {
+                    LParen => { nesting += 1 },
+                    RParen => { nesting -= 1 },
+                    _ => {},
+                }
+
+                if nesting > 0 {
+                    // Consume substream
+                    substream.push(token.clone());
+                } else {
+                    // Parse the substream and clear it
+                    consume_substream = false;
+                    list.push(Object::from_tokens(substream.clone())?);
+                    substream = Vec::new();
+                }
+            }
+        }
+
+        Ok(Object::List(list))
+    }
 }
