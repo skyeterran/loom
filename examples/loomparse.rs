35876897822a259665973c26d869a5cf5d9a6e9c
skyeterran <skyeterran@gmail.com>
2022-11-28 00:04:20 -0800

    Starting to move to a parsing model that converts dialogue to lisp expressions THEN parses those

--- examples/loomparse.rs
+++ examples/loomparse.rs
@@ -7,6 +7,22 @@ use loom::script::Script;
 fn main() {
     let source = fs::read_to_string("script.loom").expect("Couldn't load file!");
 
-    let mut script = Script::parse(source);
-    println!("{:#?}", script);
+    //let mut script = Script::parse(source);
+    //println!("{:#?}", script);
+
+    let mut new_lines: Vec<String> = Vec::new();
+    for line in source.split("\n") {
+        let raw = line.trim();
+        let Some(first_char) = raw.chars().next() else {
+            continue;
+        };
+        let Some((speaker, content)) = raw.split_once(": ") else {
+            new_lines.push(raw.to_string());
+            continue;
+        };
+        new_lines.push(format!("(say {speaker} \"{content}\")"));
+    }
+    for line in new_lines {
+        println!("{line}");
+    }
 }
