ccb832ac224486e19c5f1b9d76f0119031fc840c
skyeterran <skyeterran@gmail.com>
2023-02-23 12:18:19 -0800

    Fixed up REPL

---
+++ examples/repl.rs
@@ -0,0 +1,47 @@
+use std::env;
+use std::fs;
+use std::io::stdout;
+use std::io::{self, Write};
+use loom::parser::{tokenize, tokens_to_exp};
+use loom::exp::{LoomEnv, LoomExp, LoomErr};
+
+fn main() -> Result<(), LoomErr> {
+    let mut env = LoomEnv::default();
+
+    let mut in_buffer = String::new();
+    loop {
+        // Get user input
+        print!("> ");
+        stdout().flush().unwrap();
+        match io::stdin().read_line(&mut in_buffer) {
+            Ok(_) => {
+                let Some(input) = in_buffer.strip_suffix("\n") else {
+                    println!("No input!");
+                    break;
+                };
+                match input {
+                    "q" => {
+                        println!("Farewell, traveler!");
+                        break;
+                    },
+                    "env" => {
+                        println!("{:#?}", env);
+                    },
+                    _ => {
+                        let tokens = tokenize(input.to_string()).unwrap();
+                        let exp = tokens_to_exp(tokens).unwrap();
+                        let result = exp.eval(&mut env).unwrap();
+                        //println!("{:?}", result);
+                    },
+                }
+            }
+            Err(e) => {
+                println!("Error: {e}");
+            }
+        }
+        // Remember to clear the input buffer!
+        in_buffer.clear();
+    }
+
+    Ok(())
+}
