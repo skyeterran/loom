a09ca8d644cca53d304626b62ad615f3c8a52b7e
Skye Terran <skye@Theo.local>
2023-09-11 21:23:21 -0700

    Improved reader

--- loom_reader/examples/basic.rs
+++ loom_reader/examples/basic.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::fmt;
 use std::error::Error;
 use loom_reader::{
     parse::{
@@ -6,53 +7,84 @@ use loom_reader::{
     },
     forms::Form,
 };
+use std::collections::HashMap;
 
 #[derive(Debug)]
 enum Prim {
     Nil,
     Value(String),
-    Op { kind: String, args: Vec<usize> },
+    Op { kind: String, args: Vec<ID> },
 }
 
 #[derive(Debug)]
 struct Compiler {
-    variables: Vec<Prim>
+    variables: Vec<Prim>,
+    ids: HashMap<String, ID>, // Names to variable IDs
+}
+
+#[derive(Debug, Clone, Copy)]
+enum ID {
+    Nil,
+    Index(usize),
+}
+
+impl fmt::Display for ID {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ID::Nil => write!(f, "nil"),
+            ID::Index(i) => write!(f, "v{i}")
+        }
+    }
 }
 
 impl Compiler {
     pub fn new() -> Self {
-        Self { variables: Vec::new() }
+        Self {
+            variables: Vec::new(),
+            ids: HashMap::new(),
+        }
     }
-    pub fn add_nil(&mut self) -> usize {
-        self.variables.push(Prim::Nil);
-        self.variables.len() - 1
-    }
-    pub fn add_value(&mut self, x: String) -> usize {
+    pub fn add_value(&mut self, x: String) -> ID {
         self.variables.push(Prim::Value(x));
-        self.variables.len() - 1
+        ID::Index(self.variables.len() - 1)
     }
-    pub fn add_op(&mut self, kind: String, args: Vec<usize>) -> usize {
+    pub fn add_op(&mut self, kind: String, args: Vec<ID>) -> ID {
         self.variables.push(Prim::Op { kind, args });
-        self.variables.len() - 1
+        ID::Index(self.variables.len() - 1)
     }
-    pub fn add_exp(&mut self, x: Exp) -> usize {
+    pub fn add_exp(&mut self, x: Exp) -> ID {
         match x {
             Exp::SExp { .. } => {
-                let mut ids: Vec<usize> = Vec::new();
-                if let Some(args) = x.args() {
-                    for arg in args {
-                        self.add_exp(arg);
-                        ids.push(self.variables.len() - 1);
-                    }
-                }
                 let Some(kind) = x.car_symbol() else { todo!() };
-                self.add_op(kind, ids)
+                if kind == "def" {
+                    let Some(var_key) = x.arg_symbol(0) else { todo!() };
+                    let Some(var_exp) = x.arg(1) else { todo!() };
+                    let op_id = self.add_exp(var_exp);
+                    self.ids.insert(var_key, op_id);
+                    ID::Nil
+                } else {
+                    let mut ids: Vec<ID> = Vec::new();
+                    if let Some(args) = x.args() {
+                        for arg in args {
+                            // Check if this is a named variable
+                            if let Some(var_key) = arg.as_symbol() {
+                                if let Some(var_id) = self.ids.get(&var_key) {
+                                    ids.push(*var_id);
+                                    continue;
+                                }
+                            }
+                            self.add_exp(arg);
+                            ids.push(ID::Index(self.variables.len() - 1));
+                        }
+                    }
+                    self.add_op(kind, ids)
+                }
             }
             Exp::Atom(v) => {
                 self.add_value(v)
             }
             _ => {
-                self.add_nil()
+                ID::Nil
             }
         }
     }
@@ -75,7 +107,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             Prim::Op { kind, args } => {
                 let mut args_string = format!("");
                 for arg in args {
-                    args_string = format!("{} v{}", args_string, arg);
+                    args_string = format!("{} {}", args_string, arg);
                 }
                 format!("{kind}{args_string}")
             }
