62be41e49afd8031794887e115e302dfc6bbc87b
Skye Terran <skye@Theo.local>
2024-07-03 00:59:01 -0700

    Further work on tuber-related parse example

--- loom_reader/examples/parse.rs
+++ loom_reader/examples/parse.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::error::Error;
+use std::collections::HashMap;
 use loom_reader::{
     parse::{
         Exp, read_expressions
@@ -11,6 +12,45 @@ use loom_reader::{
 enum Prim {
     Nil,
     Keyword(String),
+    Value(String),
+    Object {
+        kind: String,
+        props: HashMap<String, Prim>,
+    },
+}
+
+#[derive(Debug)]
+struct SignalSource(String);
+
+// TODO: Build a macro reflection system for loom to use rust types?
+// There needs to be a more streamlined way of doing this - traits?
+#[derive(Debug)]
+enum Image {
+    Tex { path: String },
+    TexSequence { paths: Vec<String>, time: SignalSource },
+}
+
+impl Prim {
+    pub fn from_exp(x: Exp) -> Self {
+        match x {
+            Exp::Nil => Self::Nil,
+            Exp::Atom(contents) => {
+                if let Some(keyword) = contents.strip_prefix('@') {
+                    Self::Keyword(keyword.to_string())
+                } else {
+                    Self::Value(contents.to_string())
+                }
+            }
+            Exp::SExp { kind, args, kwargs } => {
+                let mut props: HashMap<String, Prim> = HashMap::new();
+                for (k, v) in kwargs.iter() {
+                    props.insert(k.clone(), Prim::from_exp(v.clone()));
+                }
+                Self::Object { kind: kind.as_symbol().unwrap(), props }
+            }
+            _ => Self::Nil, // TODO: Remove this
+        }
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -19,7 +59,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let expressions = read_expressions(source)?;
 
     for x in expressions {
-        println!("{:#?}", x);
+        println!("{:#?}", Prim::from_exp(x));
     }
 
     Ok(())
